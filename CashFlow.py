f06c2eb6379ff54fbbdf36f6492c008717c9b23a
exploevo <sharingevolving@gmail.com>
2022-05-03 12:30:43 +0200

    Aggiunta funzione da implementare per riempire celle vuote fornitori

--- CashFlow.py
+++ CashFlow.py
@@ -2,3 +2,15 @@ import pandas as pd
 
 cashflow = pd.read_csv('PF_Pivot1.csv', delimiter=';')
 print(cashflow.head(10))
+
+def blank_cell():
+    '''Funzione da completare per la sostituzione dei nomi nella
+    colonna fonritori dove le celle sono vuote 
+    La funzione va impelementara è stato testato solo il ciclo '''
+    for i in range(len(f)):
+        if not pd.isna(f[i]):
+            cp=f[i]
+            print(f[i])
+        else:
+            f[i]=cp
+            #print('vuota')
